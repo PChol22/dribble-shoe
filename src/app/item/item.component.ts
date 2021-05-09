1a5da4f2330621dbb2319f519b727ff56f70e78d
Pchol22 <pierre.chollet@student-cs.fr>
2021-05-09 17:27:42 +0200

    fine tuning

--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -10,10 +10,10 @@ export class ItemComponent implements OnInit {
   @Input() public color = '#FF0073';
   @Input() public imgUrl = '';
   @Input() public selected = false;
+  @Input() public last = false;
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
 }
